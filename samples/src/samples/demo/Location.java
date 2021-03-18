cd4f4249034478ae2a342acda381e716a2bcfeb9
safwan <safwanyusop220@gmail.com>
2021-03-18 19:26:46 +0800

    Initial Commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,28 @@
+package samples.demo;
+
+public class Location {
+
+	private String location;
+	private String distance;
+	private String name;
+	
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getDistance() {
+		return distance;
+	}
+	public void setDistance(String distance) {
+		this.distance = distance;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
